b102d89b71dce550b059a82b51fbe11615916297
Enison Silva Oliveira <enison.oliveira@foursys.com.br>
2025-03-29 11:32:23 -0300

    extrutural implementation

---
+++ src/main/java/com/hyperativa/payment/securecard/SecurecardApplication.java
@@ -0,0 +1,13 @@
+package com.hyperativa.payment.securecard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurecardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurecardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hyperativa/payment/securecard/SecurecardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyperativa.payment.securecard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurecardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
