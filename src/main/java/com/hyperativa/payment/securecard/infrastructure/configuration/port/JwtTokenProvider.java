3fe96a2761cf6916c17b45e2e3368ababa256ceb
Enison Silva Oliveira <enison.oliveira@foursys.com.br>
2025-03-29 15:35:11 -0300

    configurations

---
+++ src/main/java/com/hyperativa/payment/securecard/infrastructure/configuration/port/JwtTokenProvider.java
@@ -0,0 +1,11 @@
+package com.hyperativa.payment.securecard.infrastructure.configuration.port;
+
+import org.springframework.security.core.Authentication;
+
+public interface JwtTokenProvider {
+
+     public String generateToken(Authentication authentication) throws Exception;
+     public boolean validateToken(String token);
+     public Authentication getAuthentication(String token) ;
+    
+}
