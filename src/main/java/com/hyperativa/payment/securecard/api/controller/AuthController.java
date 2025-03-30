f1994892e5ca9c02cc424c86e8d10d48efdfc5ad
Enison Silva Oliveira <enison.oliveira@foursys.com.br>
2025-03-30 00:40:44 -0300

    implementation of infastructure

--- src/main/java/com/hyperativa/payment/securecard/api/controller/AuthController.java
+++ src/main/java/com/hyperativa/payment/securecard/api/controller/AuthController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -41,7 +42,7 @@ public class AuthController {
 
             return ResponseEntity.ok(new TokenResponse(token)); 
 
-        } catch (Exception e) {
+        } catch (AuthenticationException e) {
             return ResponseEntity.status(401).body(new TokenResponse(e.getMessage()));
         }
     }
